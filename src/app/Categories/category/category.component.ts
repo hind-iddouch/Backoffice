119d10f2c30831f9289603b5582084627324a0a9
Hind Iddouch <iddouch.hinde@gmail.com>
2024-05-11 16:52:26 +0200

    add

--- src/app/Categories/category/category.component.ts
+++ src/app/Categories/category/category.component.ts
@@ -73,6 +73,12 @@ updateCategories(): void {
             // Handle error appropriately
         }
     );
+
+    const modalupdate = document.getElementById('updateProductModal');
+  
+    // Check if modal exists before performing operations
+    modalupdate?.classList.add('hidden');
+    modalupdate?.setAttribute('aria-hidden', 'true');
 }
 
 
@@ -124,6 +130,41 @@ deleteCategory(id: number): void {
       modal?.classList.add('hidden');
       modal?.setAttribute('aria-hidden', 'true');
     }
+
+
+     // Variable to control the visibility of the modal
+  isModalVisible: boolean = false;
+
+  // Function to toggle the visibility of the modal
+  toggleModalUpdate() {
+    const modalupdate = document.getElementById('updateProductModal');
+    if (modalupdate) {
+      modalupdate.classList.toggle('hidden');
+    }
+  }
+  toggleModalDelete() {
+    const modalDelete = document.getElementById('deleteModal');
+    if (modalDelete) {
+      modalDelete.classList.toggle('hidden');
+    }
+  }
+
+  closeModalUpdate() {
+    // Get the modal element
+    const modalupdate = document.getElementById('updateProductModal');
+  
+    // Check if modal exists before performing operations
+    modalupdate?.classList.add('hidden');
+    modalupdate?.setAttribute('aria-hidden', 'true');
+  }
+  closeModalDelete() {
+    // Get the modal element
+    const modalDelete = document.getElementById('deleteModal');
+  
+    // Check if modal exists before performing operations
+    modalDelete?.classList.add('hidden');
+    modalDelete?.setAttribute('aria-hidden', 'true');
+  }
   
 
 }
--- src/app/Users/customers/customers.component.ts
+++ src/app/Users/customers/customers.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class CustomersComponent {
 
+
+
+  toggleModalDelete() {
+    const modalDelete = document.getElementById('deleteModal');
+    if (modalDelete) {
+      modalDelete.classList.toggle('hidden');
+    }
+  }
+
+  closeModalDelete() {
+    // Get the modal element
+    const modalDelete = document.getElementById('deleteModal');
+  
+    // Check if modal exists before performing operations
+    modalDelete?.classList.add('hidden');
+    modalDelete?.setAttribute('aria-hidden', 'true');
+  }
+  toggleModalUpdate() {
+    const modalupdate = document.getElementById('updateProductModal');
+    if (modalupdate) {
+      modalupdate.classList.toggle('hidden');
+    }
+  }
+  closeModalUpdate() {
+    // Get the modal element
+    const modalupdate = document.getElementById('updateProductModal');
+  
+    // Check if modal exists before performing operations
+    modalupdate?.classList.add('hidden');
+    modalupdate?.setAttribute('aria-hidden', 'true');
+  }
 }
